dc5b02ad78b772226f9caa5d1519a3a93695835f
Nagion <skrgus2004@naver.com>
2018-11-26 19:29:33 +0900

    Update Sort_2751_MergeSort.cpp

--- Baekjoon/Sort_2751_MergeSort.cpp
+++ Baekjoon/Sort_2751_MergeSort.cpp
@@ -4,25 +4,32 @@
 */
 #include <stdio.h>
 
+//병합 정렬
 void merge(int a[], int l, int m, int r){
+	//i : 왼쪽 배열 인덱스, j : 오른쪽 배열 인덱스, tempIdx : 임시 저장배열 인덱스
 	int i = l, j = m + 1, tempIdx = l;
 	int temp[1000001];
 
+	//왼쪽과 오른쪽 배열의 값을 비교하며 작은 값으 임시 배열에 저장
 	while(i <= m && j <= r){
 		if(a[i] > a[j]) temp[tempIdx++] = a[j++];
 		else temp[tempIdx++] = a[i++];
 	}
 
+	//남은 배열의 경우 순서대로 임시 배열에 저장
 	if(i <= m) for(int k = i; k <= m; k++) temp[tempIdx++] = a[k];
 	else if(j <= r) for(int k = j; k <= r; k++) temp[tempIdx++] = a[k];
 
+	//원래 배열로 옮기기
 	for(int k = l; k <= r; k++) a[k] = temp[k];
 }
 
+//나누기
 void merge_sort(int a[], int l, int r){
 	if(l < r){
 		int m = (l + r) / 2;
 
+		//반씩 나눔
 		merge_sort(a, l, m);
 		merge_sort(a, m + 1, r);
 
